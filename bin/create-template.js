f9cd10f0fc96bd47faa45d6da82e1bba4d7f6173
caokejian <1849201815@qq.com>
2024-06-27 01:31:11 +0800

    feat: 添加props控制和更新模版支持sb框架

--- bin/create-template.js
+++ bin/create-template.js
@@ -5,41 +5,41 @@ const path = require('path');
 const readline = require('readline');
 // 使用动态import导入inquirer模块
 const main = async () => {
-    let inquirer;
-    try {
-        // 动态导入inquirer模块
-        const { default: prompt } = await import('inquirer');
-        inquirer = prompt;
-    } catch (err) {
-        console.error('无法加载inquirer模块:', err);
-        process.exit(1);
+  let inquirer;
+  try {
+    // 动态导入inquirer模块
+    const { default: prompt } = await import('inquirer');
+    inquirer = prompt;
+  } catch (err) {
+    console.error('无法加载inquirer模块:', err);
+    process.exit(1);
+  }
+
+  const promptOptions = [
+    {
+      type: 'list',
+      name: 'action',
+      message: '请选择要执行的操作:',
+      choices: [
+        '创建nuwa（女娲）-React组件',
+        '测试（误用）'
+      ]
     }
+  ];
 
-    const promptOptions = [
-        {
-            type: 'list',
-            name: 'action',
-            message: '请选择要执行的操作:',
-            choices: [
-                '创建nuwa（女娲）-React组件',
-                '测试（误用）'
-            ]
-        }
-    ];
+  try {
+    // 显示命令行选项
+    const answers = await inquirer.prompt(promptOptions);
 
-    try {
-        // 显示命令行选项
-        const answers = await inquirer.prompt(promptOptions);
-
-        if (answers.action === '创建nuwa（女娲）-React组件') {
-            askComponentName();
-        } else if (answers.action === '测试（误用）') {
-            console.log('执行测试操作...');
-            // 在这里添加测试操作的代码
-        }
-    } catch (error) {
-        console.error('发生错误:', error);
+    if (answers.action === '创建nuwa（女娲）-React组件') {
+      askComponentName();
+    } else if (answers.action === '测试（误用）') {
+      console.log('执行测试操作...');
+      // 在这里添加测试操作的代码
     }
+  } catch (error) {
+    console.error('发生错误:', error);
+  }
 };
 
 function verify() {
@@ -57,24 +57,25 @@ main();
 
 // 提示用户输入组件名称的方法
 function askComponentName() {
-    const readline = require('readline').createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
+  const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
 
-    readline.question('新创建组件的名字: ', (componentName) => {
-        const directory = path.join(process.cwd(), componentName);
+  readline.question('新创建组件的名字: ', (componentName) => {
+    const directory = path.join(process.cwd(), componentName);
+    const modelDirectory = path.join(directory, 'model');
 
-        if (fs.existsSync(directory)) {
-            console.error('\x1b[1m\x1b[33m%s 组件已存在，请重新创建新的组件\x1b[0m', componentName);
-            readline.close();
-            return;
-        }
+    if (fs.existsSync(directory)) {
+      console.error('\x1b[1m\x1b[33m%s 组件已存在，请重新创建新的组件\x1b[0m', componentName);
+      readline.close();
+      return;
+    }
 
-        fs.mkdirSync(directory, { recursive: true });
-
-        const templates = {
-            'index.html': `<!DOCTYPE html>
+    fs.mkdirSync(directory, { recursive: true });
+    fs.mkdirSync(modelDirectory, { recursive: true });
+    const templates = {
+      'index.html': `<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
@@ -85,28 +86,61 @@ function askComponentName() {
   <div id="app"></div>
 </body>
 </html>`,
-            'index.tsx': `import React from 'react'
-import { createRoot } from 'react-dom/client'
-            
-const NewDemo: React.FC = () => {
-  return <Upload />
-}
-
-if(process.env.APP_NAME !== 'XLP') {
-  createRoot(document.getElementById('app')!).render(<NewDemo />)
-}
-            
-export { NewDemo }`,
-            'meta.json': `{
+      'index.tsx': `import React from 'react';
+      import * as s from './index.less'
+      import xprops from './model/props'
+      interface ButtonProps {
+        label?: string;
+        onClick?: () => void;
+      }
+      
+      const mergeProps = (xprops: ButtonProps, props: ButtonProps) => ({
+        ...xprops,
+        ...props,
+      })
+      export const Index = (props: ButtonProps) => {
+      
+        const mergedProps = mergeProps(xprops, props);
+        return (
+          <button
+            type="button"
+            className={s.button}
+            {...mergedProps}
+          >
+            {mergedProps.label || '默认文字'}
+          </button>
+        )
+      }
+      `,
+      'index.less': `{
+        .button {
+          color: white;
+          background-color: #1ea7fd;
+          width: 300px;
+          height: 60px;
+          line-height: 60px;
+          font-size: 16px;
+          border: none;
+          cursor: pointer;
+          font-weight: 700;
+          font-family: 'Nunito Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+        }
+      }`,
+      'meta.json': `{
   "desc": "这是一个组件",
   "img": "图片链接"
 }`
-        };
+    };
 
-        for (const [filename, content] of Object.entries(templates)) {
-            fs.writeFileSync(path.join(directory, filename), content);
-        }
-        console.log('\x1b[1m\x1b[33mComponent %s created in %s\x1b[0m', componentName, directory);
-        readline.close();
-    });
+    const propsContent = `export default {
+      label: 'ckj is king'
+    };`;
+    fs.writeFileSync(path.join(modelDirectory, 'props.js'), propsContent);
+
+    for (const [filename, content] of Object.entries(templates)) {
+      fs.writeFileSync(path.join(directory, filename), content);
+    }
+    console.log('\x1b[1m\x1b[33mComponent %s created in %s\x1b[0m', componentName, directory);
+    readline.close();
+  });
 }
