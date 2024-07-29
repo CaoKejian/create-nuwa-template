a4bff150d059ddc8cbc259e826a621950329308c
Cao Kejian <colinccao@tencent.com>
2024-07-30 00:13:23 +0800

    feat: 添加发包函数

--- bin/publish.js
+++ bin/publish.js
@@ -20,10 +20,9 @@ function componentPublish() {
   const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'))
   const packageName = packageJson.name
 
-  console.log(packageName);
   // 获取当前版本
   const currentVersion = execSync(`npm show ${packageName} version`, { encoding: 'utf8' }).trim()
-  console.log(`当前包版本：${packageName}@${currentVersion}`)
+  console.log(`\x1b[1;33m当前包版本：${packageName}@${currentVersion}\x1b[0m`);
 
   // 显示标题
   title(`更新 ${packageName} 版本`)
@@ -56,19 +55,45 @@ function componentPublish() {
         console.log('无效选择, 默认更新小版本。')
         versionType = 'patch'
     }
+    if (!isGitWorkingDirectoryClean()) {
+      console.error('\x1b[1;31m错误：Git 分支有未提交内容，请保证 Git 分支干净后再发布。\x1b[0m');
+      rl.close();
+      process.exit(1)
+    }
 
-    // 更新版本
-    execSync(`npm version ${versionType}`, { stdio: 'inherit' })
+    try {
+      // 更新版本
+      execSync(`npm version ${versionType}`, { stdio: 'inherit' })
 
-    // 发布到本地 registry
-    execSync('npm publish --registry http://localhost:4873/', { stdio: 'inherit' })
+      // 发布到本地 registry
+      execSync('npm publish --registry http://localhost:4873/', { stdio: 'inherit' })
 
-    // 获取更新后的版本
-    const updatedVersion = execSync(`npm show ${packageName} version`, { encoding: 'utf8' }).trim()
-    console.log(`\x1b[133m更新完成：${packageName}@${updatedVersion}\x1b[0m`)
+      // 获取更新后的版本
+      const updatedVersion = execSync(`npm show ${packageName} version`, { encoding: 'utf8' }).trim()
+      const versionInfo = `${packageName}@${updatedVersion}`;
 
-    rl.close()
+      // 将版本信息复制到剪贴板
+      clipboardy.writeSync(versionInfo);
+      console.log(`\x1b[1;32m组件更新成功, 版本信息已复制到剪贴板：${versionInfo}\x1b[0m`);
+    } catch(err) {
+      console.error(`\x1b[1;31m组件更新失败:, ${err.message} \x1b[0m`);
+      process.exit(1);
+    } finally {
+      rl.close()
+    }
   })
 }
+
+// 检查 Git 工作目录状态
+function isGitWorkingDirectoryClean() {
+  try {
+    const status = execSync('git status --porcelain', { encoding: 'utf8' }).trim();
+    return status === '';
+  } catch (error) {
+    console.error(`\x1b[1;31m无法检查 Git 工作目录状态:, ${err.message} \x1b[0m`);
+    process.exit(1);
+  }
+}
+
 componentPublish()
 module.exports = { componentPublish }
